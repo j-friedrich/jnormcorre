205df5c03bd09d20891be13caca206c877ddc07e
Johannes Friedrich <johannes.friedrich@alleninstitute.org>
2023-05-25 11:47:57 -0700

    change np.int->int, ... to be compatible with numpy 1.24

--- jnormcorre/motion_correction.py
+++ jnormcorre/motion_correction.py
@@ -401,7 +401,7 @@ class MotionCorrect(object):
         else:
             self.motion_correct_rigid(template=template, save_movie=save_movie)
             b0 = np.ceil(np.max(np.abs(self.shifts_rig)))
-        self.border_to_0 = b0.astype(np.int)
+        self.border_to_0 = b0.astype(int)
         self.mmap_file = self.fname_tot_els if self.pw_rigid else self.fname_tot_rig
         return self
 
@@ -536,9 +536,9 @@ def apply_shift_iteration(img, shift, border_nan:bool=False, border_type=cv2.BOR
     if border_nan is not False:
         max_w, max_h, min_w, min_h = 0, 0, 0, 0
         max_h, max_w = np.ceil(np.maximum(
-            (max_h, max_w), shift)).astype(np.int)
+            (max_h, max_w), shift)).astype(int)
         min_h, min_w = np.floor(np.minimum(
-            (min_h, min_w), shift)).astype(np.int)
+            (min_h, min_w), shift)).astype(int)
         if border_nan is True:
             img[:max_h, :] = np.nan
             if min_h < 0:
@@ -592,7 +592,7 @@ def bin_median(mat, window=10, exclude_nans=True):
     T, d1, d2 = np.shape(mat)
     if T < window:
         window = T
-    num_windows = np.int(old_div(T, window))
+    num_windows = T // window
     num_frames = num_windows * window
     if exclude_nans:
         img = np.nanmedian(np.nanmean(np.reshape(
